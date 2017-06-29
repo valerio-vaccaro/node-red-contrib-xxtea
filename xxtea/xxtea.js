49862c7f7ae6bb8afbc9b36e8ba04a3785e804ed
valerio-vaccaro <valerio.vaccaro@gmail.com>
2017-06-29 08:53:33 +0200

    removed password and solved errors in decryption process

--- xxtea/xxtea.js
+++ xxtea/xxtea.js
@@ -21,8 +21,8 @@
 // import the requirements
 const xxtea = require('xxtea');
 
-module.exports = function (RED) {
-  // Node for Sign a generic payload
+module.exports = function(RED) {
+	// Node for Sign a generic payload
 	function xxtea_encrypt(n) {
 		RED.nodes.createNode(this, n);
 		this.status({
@@ -32,10 +32,11 @@ module.exports = function (RED) {
 		});
 		const msg = {};
 		const node = this;
-		this.on('input', function (msg) {
-      // To encrypt
+		this.on('input', function(msg) {
+			// To encrypt
 			const encrypted = xxtea.encrypt(msg.payload, msg.password);
 			msg.payload = encrypted;
+			msg.password = null;
 			node.send(msg);
 			this.status({
 				fill: 'grey',
@@ -44,7 +45,7 @@ module.exports = function (RED) {
 			});
 		});
 	}
-  // Node for Sign a generic payload
+	// Node for Sign a generic payload
 	function xxtea_decrypt(n) {
 		RED.nodes.createNode(this, n);
 		this.status({
@@ -54,10 +55,11 @@ module.exports = function (RED) {
 		});
 		const msg = {};
 		const node = this;
-		this.on('input', function (msg) {
-      // To decrypt
+		this.on('input', function(msg) {
+			// To decrypt
 			const decrypted = xxtea.decrypt(msg.payload, msg.password);
-			msg.payload = encrypted;
+			msg.payload = decrypted;
+			msg.password = null;
 			node.send(msg);
 			this.status({
 				fill: 'grey',
@@ -67,8 +69,8 @@ module.exports = function (RED) {
 		});
 	}
 
-  // Register the node by name. This must be called before overriding any of the
-  // Node functions.
+	// Register the node by name. This must be called before overriding any of the
+	// Node functions.
 	RED.nodes.registerType('xxtea_encrypt', xxtea_encrypt);
 	RED.nodes.registerType('xxtea_decrypt', xxtea_decrypt);
 };
